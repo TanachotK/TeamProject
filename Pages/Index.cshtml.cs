689e771777296bedd23b08a624c1dd97067dbcaa
Tanachot <tanachoty0000@gmail.com>
2026-03-30 10:24:00 +0200

    Fixing shit

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,8 +6,15 @@ namespace TeamProject.Pages;
 public class IndexModel : PageModel
 {
     public Models.FilmModels FilmData { get; set; } = new Models.FilmModels();
+
     public void OnGet()
     {
 
+    }
+
+    public void OnPost()
+    {
+        // Handle form submission, e.g., save to database or process data
+        // For now, just leave it as is or add logic here
     }
 }
