cf0bd912325d1245cabddd1dbf9de159af0e22eb
Tanachot <tanachoty0000@gmail.com>
2026-04-20 09:36:58 +0200

    Fix

--- obj/Debug/net10.0/TeamProject.AssemblyInfo.cs
+++ obj/Debug/net10.0/TeamProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeamProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e61ad29a2d656fb56c86282e06d6af1e387f892")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a74998411460406011cac6761eb60076d9d82524")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeamProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeamProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
