aa1e1000e3fc1d29263a5da474faf3354e172e4b
R41ph21 <ralph.schaur@angeskola.se>
2026-04-21 09:58:44 +0200

    F

--- obj/Debug/net10.0/TeamProject.AssemblyInfo.cs
+++ obj/Debug/net10.0/TeamProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeamProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdaade4d708cba0b1ed2aaadf072ec73b8322548")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90065123af9c27c86a6824156ab58985a7acebf5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeamProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeamProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
